777b8db3177e3598431a57421a46129bdbe67128
imran <imrandabc@gmail.com>
2016-11-26 16:20:59 +0530

    0# Added model class

---
+++ src/main/java/com/aero/model/Drug.java
@@ -0,0 +1,24 @@
+package com.aero.model;
+
+public class Drug {
+
+	private String drugIdentifier;
+	private String name;
+
+	public String getDrugIdentifier() {
+		return drugIdentifier;
+	}
+
+	public void setDrugIdentifier(String drugIdentifier) {
+		this.drugIdentifier = drugIdentifier;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
